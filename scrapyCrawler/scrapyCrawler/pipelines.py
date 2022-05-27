1ba6aabbcff649de45726f6f1aab85012c6317e7
Anton <nyagolov.anton@gmail.com>
2022-05-28 02:43:33 +0300

    Setup pipeline for processed items

--- scrapyCrawler/scrapyCrawler/pipelines.py
+++ scrapyCrawler/scrapyCrawler/pipelines.py
@@ -1,13 +1,39 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import sqlite3
 
-
-# useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
 
-class ScrapycrawlerPipeline:
+class ProductPipeline(object):
+    def __init__(self):
+        self.create_connection()
+
+
+    def create_connection(self):
+        self.conn = sqlite3.connect('products.db')
+        self.curr = self.conn.cursor()
+
+
+    def store_to_db(self, item):
+        self.curr.execute("""
+            INSERT INTO products_tb
+            VALUES (?, ?, ?, ?, ?, ?)
+            ON CONFLICT(product_number) DO UPDATE
+            SET title = ?, subtitle = ?, price = ?
+                            """,(
+                                    item['title'],
+                                    item['category'],
+                                    item['sub_category'],
+                                    item['subtitle'],
+                                    item['product_number'],
+                                    item['price'],
+                                    item['title'],
+                                    item['subtitle'],
+                                    item['price'],
+                                )
+        )
+        self.conn.commit()
+
+
     def process_item(self, item, spider):
+        self.store_to_db(item)
         return item
