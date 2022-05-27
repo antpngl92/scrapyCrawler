899f7cad749ae255b85c185d57b9c0a21be2ac04
Anton <nyagolov.anton@gmail.com>
2022-05-28 02:00:50 +0300

    Create Database setup script

---
+++ scrapyCrawler/scrapyCrawler/database_setup.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+connection = sqlite3.connect('products.db')
+currsor = connection.cursor()
+
+currsor.execute("""create table products_tb(
+                    title text,
+                    category text,
+                    sub_category text,
+                    subtitle text,
+                    product_number text PRIMARY KEY,
+                    price decimal(10,2)
+                )""")
+
+
+connection.commit()
+connection.close()
