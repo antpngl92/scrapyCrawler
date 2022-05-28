5ba7e942020331d380fec7cfc93485c858fd81e7
Anton <nyagolov.anton@gmail.com>
2022-05-28 03:11:09 +0300

    Fix price rounding bug

--- scrapyCrawler/scrapyCrawler/spiders/product_spider.py
+++ scrapyCrawler/scrapyCrawler/spiders/product_spider.py
@@ -9,20 +9,22 @@ class ProductSpider(scrapy.Spider):
     name = 'products'
 
     start_urls = [
-        'https://gplay.bg/hama-sonic-mobil-183'
+        'https://gplay.bg/creative-sound-blaster-ae-7',
+        'https://gplay.bg/estillo-8801',
+        'https://gplay.bg/nitro-concepts-d12',
     ]
 
     def parse(self, response):
         products = ProductItem()
 
         title = response.css('h1.large-title::text').extract()[0].strip('\n')
-        __breadcrum_paths = response.css('div.path a::attr(href)').extract()
-        category = __breadcrum_paths[1].strip(DOMAIN + '/')
-        sub_category = __breadcrum_paths[2].strip(DOMAIN + '/')
+        breadcrum_paths = response.css('div.path a::attr(href)').extract()
+        category = breadcrum_paths[1].strip(DOMAIN + '/')
+        sub_category = breadcrum_paths[2].strip(DOMAIN + '/')
         subtitle = response.css('h2.product-subtitle::text').extract()[0].strip('\n')
         product_number = response.css('div.product-ref-number strong::text').extract()[0]
-        __price_element = response.css('div.normal-price price').extract()[0]
-        price = __price_element.split("\"")[1]
+        price_element = response.css('div.normal-price price').extract()[0]
+        price = round(float(price_element.split("\"")[1]),2)
 
         products['title'] = title
         products['category'] = category
